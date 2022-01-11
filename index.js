1d28ddbc19197e3f78da6c32866ec55fff0e6937
aminespon <89251553+aminespon@users.noreply.github.com>
2022-01-11 22:38:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => console.log("DB Connection Successfull!"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
